3ba89f522b9cd23add7c0765880cfc89f85c3a82
Cabe Bartz <cabebartz@fullsai.edu>
2012-07-28 17:24:43 -0700

    adding files

---
+++ js/json.js
@@ -0,0 +1,202 @@
+var json = {
+	"bill1": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Bank Of America"],
+		"accountNum": ["Account Number:", "12345"],
+		"billAmount": ["Amount Due:", "125.00"],
+		"dueDate": ["Date Due:", "2012-10-11"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "5"],
+		"comments": ["comments:", "already paid"]
+	},
+	"bill2": {
+		"category": ["Bill Category:", "Rent"],
+		"billName": ["Bill Name:", "KPM"],
+		"accountNum": ["Account Number:", "1234589"],
+		"billAmount": ["Amount Due:", "1125.00"],
+		"dueDate": ["Date Due:", "2012-10-15"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "9"],
+		"comments": ["comments:", "Pay Soon"]
+	},
+	"bill3": {
+		"category": ["Bill Category:", "Utilities"],
+		"billName": ["Bill Name:", "PSE"],
+		"accountNum": ["Account Number:", "4589"],
+		"billAmount": ["Amount Due:", "89.98"],
+		"dueDate": ["Date Due:", "2012-12-12"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "7"],
+		"comments": ["comments:", "yea its paid"]
+	},
+	"bill4": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Everything"],
+		"accountNum": ["Account Number:", "4876"],
+		"billAmount": ["Amount Due:", "300.89"],
+		"dueDate": ["Date Due:", "2012-12-13"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "2"],
+		"comments": ["comments:", "Everything bill"]
+	},
+	"bill5": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Chase"],
+		"accountNum": ["Account Number:", "55555"],
+		"billAmount": ["Amount Due:", "100.34"],
+		"dueDate": ["Date Due:", "2012-09-09"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "4"],
+		"comments": ["comments:", "Visa"]
+	},
+	"bill6": {
+		"category": ["Bill Category:", "Utilities"],
+		"billName": ["Bill Name:", "CNG"],
+		"accountNum": ["Account Number:", "4444"],
+		"billAmount": ["Amount Due:", "89.98"],
+		"dueDate": ["Date Due:", "2012-10-10"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "8"],
+		"comments": ["comments:", "Natural Gas"]
+	},
+	"bill7": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Food"],
+		"accountNum": ["Account Number:", "66666"],
+		"billAmount": ["Amount Due:", "329.65"],
+		"dueDate": ["Date Due:", "2012-11-11"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "9"],
+		"comments": ["comments:", "groceries"]
+	},
+	"bill8": {
+		"category": ["Bill Category:", "Utilities"],
+		"billName": ["Bill Name:", "Comcast"],
+		"accountNum": ["Account Number:", "7777"],
+		"billAmount": ["Amount Due:", "123.23"],
+		"dueDate": ["Date Due:", "2012-08-08"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "1"],
+		"comments": ["comments:", "Cable"]
+	},
+	"bill9": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Car Maintenance"],
+		"accountNum": ["Account Number:", "1111"],
+		"billAmount": ["Amount Due:", "50.43"],
+		"dueDate": ["Date Due:", "2012-01-10"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "5"],
+		"comments": ["comments:", "Oil changes"]
+	},
+	"bill10": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Mastercard"],
+		"accountNum": ["Account Number:", "22222"],
+		"billAmount": ["Amount Due:", "25.89"],
+		"dueDate": ["Date Due:", "2012-02-02"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "4"],
+		"comments": ["comments:", "Mastercard"]
+	},
+	"bill11": {
+		"category": ["Bill Category:", "Utilities"],
+		"billName": ["Bill Name:", "Verizon"],
+		"accountNum": ["Account Number:", "3333"],
+		"billAmount": ["Amount Due:", "100.22"],
+		"dueDate": ["Date Due:", "2012-04-04"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "7"],
+		"comments": ["comments:", "Cell Phone"]
+	},
+	"bill12": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Fun"],
+		"accountNum": ["Account Number:", "55567"],
+		"billAmount": ["Amount Due:", "75.00"],
+		"dueDate": ["Date Due:", "2012-12-30"],
+		"billPaid": ["Is Bill Paid?", " Not Paid"],
+		"priority": ["Priority:", "1"],
+		"comments": ["comments:", "Do something fun"]
+	},
+	"bill13": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "American Express"],
+		"accountNum": ["Account Number:", "6767"],
+		"billAmount": ["Amount Due:", "50.00"],
+		"dueDate": ["Date Due:", "2012-12-10"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "1"],
+		"comments": ["comments:", "American Express"]
+	},
+	"bill14": {
+		"category": ["Bill Category:", "Utilities"],
+		"billName": ["Bill Name:", "CenturyTel"],
+		"accountNum": ["Account Number:", "45622"],
+		"billAmount": ["Amount Due:", "49.99"],
+		"dueDate": ["Date Due:", "2012-12-11"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "5"],
+		"comments": ["comments:", "Home Phone"]
+	},
+	"bill15": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Furniture"],
+		"accountNum": ["Account Number:", "6789"],
+		"billAmount": ["Amount Due:", "100.00"],
+		"dueDate": ["Date Due:", "2012-12-17"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "2"],
+		"comments": ["comments:", "Save money for furniture"]
+	},
+	"bill16": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Best Buy"],
+		"accountNum": ["Account Number:", "8789"],
+		"billAmount": ["Amount Due:", "100.45"],
+		"dueDate": ["Date Due:", "2012-12-30"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "4"],
+		"comments": ["comments:", "Stop using this card!"]
+	},
+	"bill17": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Group Health"],
+		"accountNum": ["Account Number:", "9090"],
+		"billAmount": ["Amount Due:", "315.90"],
+		"dueDate": ["Date Due:", "2012-12-01"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "9"],
+		"comments": ["comments:", "family healthcare"]
+	},
+	"bill18": {
+		"category": ["Bill Category:", "Credit"],
+		"billName": ["Bill Name:", "Make Believe Store"],
+		"accountNum": ["Account Number:", "7545"],
+		"billAmount": ["Amount Due:", "45.56"],
+		"dueDate": ["Date Due:", "2012-12-16"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "1"],
+		"comments": ["comments:", "This bill is made up"]
+	},
+	"bill19": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "Gadget Money"],
+		"accountNum": ["Account Number:", "3212"],
+		"billAmount": ["Amount Due:", "50.00"],
+		"dueDate": ["Date Due:", "2012-12-30"],
+		"billPaid": ["Is Bill Paid?", "Not Paid"],
+		"priority": ["Priority:", "1"],
+		"comments": ["comments:", "Saving money for gadgets"]
+	},
+	"bill20": {
+		"category": ["Bill Category:", "Misc"],
+		"billName": ["Bill Name:", "College"],
+		"accountNum": ["Account Number:", "67890"],
+		"billAmount": ["Amount Due:", "250.87"],
+		"dueDate": ["Date Due:", "2012-12-02"],
+		"billPaid": ["Is Bill Paid?", "Paid"],
+		"priority": ["Priority:", "6"],
+		"comments": ["comments:", "Money for College"]
+	}
+}
